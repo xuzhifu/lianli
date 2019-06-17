3bc1a588071ab9b9365be183dcda1612bcbfbc1a
wakakay <649865915@qq.com>
2019-06-17 21:41:51 +0800

    999

--- src/pages/detail.vue
+++ src/pages/detail.vue
@@ -7,23 +7,17 @@
             <a>XXXXXXXXXXXXXXX</a>
         </div>
 
-        <el-carousel :interval="interval" type="card" arrow="always" trigger="click">
-            <el-carousel-item>
-                <div class="cell-thumb" style="background-image:url(http://wakakay.com/images/swipe.png)"></div>
-            </el-carousel-item>
-            <el-carousel-item>
-                <div class="cell-thumb" style="background-image:url(http://wakakay.com/images/swipe.png)"></div>
-            </el-carousel-item>
-            <el-carousel-item>
-                <div class="cell-thumb" style="background-image:url(http://wakakay.com/images/swipe.png)"></div>
+        <el-carousel :height="swipeHeight" :interval="interval" type="card" arrow="always"  trigger="click">
+            <el-carousel-item v-for="(item, index) in swipeList" :key="index">
+                <div class="cell-thumb" :style="'background-image:url('+ item.url +')'"></div>
             </el-carousel-item>
         </el-carousel>
 
-        <div class="ui-swipe" :auto="interval">
+        <div class="ui-swipe" :style="'height:'+swipeHeight" :auto="interval">
             <mt-swipe>
-                <mt-swipe-item><div class="cell-thumb" style="background-image:url(http://wakakay.com/images/swipe.png)"></div></mt-swipe-item>
-                <mt-swipe-item><div class="cell-thumb" style="background-image:url(http://wakakay.com/images/swipe.png)"></div></mt-swipe-item>
-                <mt-swipe-item><div class="cell-thumb" style="background-image:url(http://wakakay.com/images/swipe.png)"></div></mt-swipe-item>
+                <mt-swipe-item v-for="(item, index) in swipeList" :key="index">
+                    <div class="cell-thumb" :style="'background-image:url('+ item.url +')'"></div>
+                </mt-swipe-item>
             </mt-swipe>
         </div>
 
@@ -80,8 +74,30 @@
 
         data() {
             return {
+                isPc: () => {
+                    let userAgentInfo = navigator.userAgent;
+                    let Agents = ["Android", "iPhone",
+                        "SymbianOS", "Windows Phone",
+                        "iPad", "iPod"];
+                    let flag = true;
+                    for (var v = 0; v < Agents.length; v++) {
+                        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    return flag;
+                },
                 interval: 5000, // 轮番图自动播播放的间隔时间
                 activeName: '2',
+                swipeHeight: '0px',
+                swipeList: [
+                    {url: 'https://lotus-carp.com/images/201945031978.jpg'},
+                    {url: 'https://lotus-carp.com/images/201945031220.jpg'},
+                    {url: 'https://lotus-carp.com/images/201945031906.jpg'},
+                    {url: 'https://lotus-carp.com/images/201945031355.jpg'},
+                    {url: 'https://lotus-carp.com/images/20194503165.jpg'}
+                ],
                 productList: [
                     {title: '上品优喜台州星光耀广场店', thumb: 'http://wakakay.com/images/product-01.png'},
                     {title: '上品优喜台州星光耀广场店', thumb: 'http://wakakay.com/images/product-02.png'},
@@ -93,8 +109,40 @@
             };
         },
 
-        methods: {
+        mounted() {
+            let self = this
+            self.__preload()
+        },
 
+        methods: {
+            __preload() {
+                let self = this
+                let count = self.swipeList.length
+                let imagesInfo = []
+
+                for (let img of self.swipeList) {
+                    let image = new Image()
+                    image.src = img.url
+                    image.onload = (info) => {
+                        imagesInfo.push({
+                            width: info.path[0].width,
+                            height: info.path[0].height
+                        })
+                        count--
+                        if(!count) {
+                            let max = _.max(imagesInfo, (item) => {
+                                return item.width;
+                            })
+
+                            if (!self.isPc()) {
+                                self.swipeHeight = Math.ceil((window.innerWidth / max.width) * max.height) + 'px'
+                            } else {
+                                self.swipeHeight = Math.ceil((window.innerWidth / 2 / max.width) * max.height) + 'px'
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 </script>
